ccb39047f8e68402555fcfe92f43d653fb863a93
guigail <guillaume.crespel@gmail.com>
2017-04-22 20:15:35 +0200

    Amazing component

--- src/app.js
+++ src/app.js
@@ -6,16 +6,9 @@ export function App (sources) {
 
   const instrument = Instrument({DOM$: sources.DOM$})
 
-  const music$ = xs.periodic(200).map(() => {
-    return {
-      frequency: (Math.random() * 1000) + 300,
-      time: (Math.random() * 1000) + 200,
-    }
-  })
-
   const sinks = {
     DOM$: instrument.DOM$,
-    MUSIC$: music$,
+    MUSIC$: instrument.MUSIC$,
   }
 
   return sinks
--- src/components/instrument/instrument.js
+++ src/components/instrument/instrument.js
@@ -1,21 +1,30 @@
-import {button, makeDOMDriver} from '@cycle/dom'
-//import xs from 'xstream'
+import {button} from '@cycle/dom'
+import xs from 'xstream'
 
 export default ({DOM$}) => {
   const componentClass = '.instrument'
+
+  const props$ = xs.of({className: componentClass, value: 2000})
 
   // listen click event
   const clickValue$ = DOM$
     .select(componentClass)
     .events('click')
-    .map(() => Math.random())
-    .startWith(10)
+    .map(() => (Math.random()*1000)+200)
 
-  // change label when clicked
-  const vdom$ = clickValue$
-        .map(label => button(componentClass, label))
+  const value$ = xs.merge(
+    props$.map(props => props.value),
+    clickValue$
+  )
+
+  const vdom$ = xs.combine(props$, value$)
+    .map(([props, value]) => button(props.className, value))
+
+  const music$ = clickValue$
+    .map(frequency => ({frequency}))
 
   return {
     DOM$: vdom$,
+    MUSIC$: music$,
   }
 }
