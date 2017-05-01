c7e44290c59136aaffdb77d06360acbfab4247ae
guigail <guillaume.crespel@gmail.com>
2017-05-01 20:04:03 +0200

    add dom to character component

--- src/components/character/character.js
+++ src/components/character/character.js
@@ -4,8 +4,19 @@ import xs from 'xstream'
 export default ({ NOTE$, props$ }) => {
   const className = '.character'
 
-  const vdom$ = props$
-    .map(props => p(className, props.name))
+  const component = (props, isNotify) => p(
+    `${className} ${isNotify && '.notify'}`,
+    // FIXME : to css file.
+    isNotify ? { style: { color: 'red' } } : { style: { color: 'black' } },
+    props && `${props.name} with ${props.instrument}`)
+
+  const animate$ = NOTE$.map(n => n.duration).flatten()
+
+  const vdom$ = xs.combine(NOTE$, props$, animate$)
+    .map(([note, props, animate]) => p(
+      component(props, note.instrument === props.instrument && animate < 100)),
+    )
+    .startWith(component())
 
   const music$ = xs.combine(NOTE$, props$)
     .filter(([note, props]) => note.instrument === props.instrument)
