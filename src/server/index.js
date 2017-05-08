91abf14d350769844ba3f4fabb54557ba50d2033
Fabien JUIF <fabien.juif@zenika.com>
2017-05-08 15:16:44 +0200

    port var env

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,12 @@ const Koa = require('koa')
 const api = require('./api')
 const statics = require('./statics')
 
+const PORT = process.env.PORT || 3000
+
 const app = new Koa()
 
 statics(app)
 api(app)
 
-app.listen(3000)
+app.listen(PORT)
+console.log(`Listening to ${PORT}`) // eslint-disable-line
