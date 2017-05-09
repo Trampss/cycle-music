0f07459d207b85bf580c78e11c3fcba4f8088de4
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2017-05-09 08:36:14 +0200

    :hammer: refacto World

--- src/components/world/world.js
+++ src/components/world/world.js
@@ -3,38 +3,76 @@ import { div } from '@cycle/dom'
 import isolate from '@cycle/isolate'
 import Rythmbox from '../rythmbox/index'
 import Character from '../character/index'
+import Wire from '../wire'
 import Speaker from '../speaker/index'
 import { CHARACTERS } from '../../config'
 
 export default ({ DOM$ }) => {
+  /*
+  Create Rythmbox
+   */
   const rythmbox = Rythmbox({ DOM$, props$: xs.of(CHARACTERS) })
 
-  const characters = CHARACTERS.map(props =>
-    isolate(Character, `${props.name}-${props.instrument}`)(
-      {
-        NOTE$: rythmbox.NOTE$,
-        props$: xs.of(props),
-      },
-    ))
+  /*
+  Connect Character with Rythmbox by Wire
+ */
+  const connectedCharacters = CHARACTERS
+    .map(props => ({
+      wireNote: isolate(Wire, `wireNote-${props.name}`)({ NOTE$: rythmbox.NOTE$ }),
+      props,
+    }))
+    .map(({ wireNote, props }) => {
+      const character = isolate(Character, `character-${props.name}`)(
+        {
+          NOTE$: wireNote.NOTE$,
+          props$: xs.of(props),
+        },
+      )
 
-  const speaker = Speaker({ MUSIC$: xs.merge(...characters.map(c => c.MUSIC$)) })
+      return Object.assign({}, { wireNote, props }, { character })
+    })
+    .map(({ wireNote, character, props }) => {
+      const wireMusic = isolate(Wire, `wireMusic-${props.name}`)({ MUSIC$: character.MUSIC$ })
 
-  const charactersDom$ = xs
-    .combine(...characters.map(c => c.DOM$))
-    .map(c => div('.characters', c))
+      return Object.assign({}, { wireNote, character, props }, { wireMusic })
+    })
+
+  /*
+  Create Speaker
+   */
+  const speaker = Speaker({
+    MUSIC$: xs.merge(...connectedCharacters.map(({ wireMusic }) => wireMusic.MUSIC$)),
+  })
+
+  /*
+  Draw DOM with all Component
+   */
+  const characters = div(
+    '.characters',
+    connectedCharacters.map(({ wireNote, character, wireMusic }) =>
+      div('.character', xs.combine(wireNote.DOM$, character.DOM$, wireMusic.DOM$).debug())),
+  )
 
   const vdom$ = xs
     .combine(
       rythmbox.DOM$,
-      charactersDom$,
+      xs.of(connectedCharacters),
       speaker.DOM$,
     )
-    .map(components => div(components))
-
-  const music$ = speaker.MUSIC$
+    .map(([rd, c, sd]) =>
+      div([
+        rd,
+        div(
+          '.characters',
+          c.map(({ wireNote, character, wireMusic }) =>
+            div('.character', xs.combine(wireNote.DOM$, character.DOM$, wireMusic.DOM$))),
+        ),
+        sd,
+      ]))
 
   return {
     DOM$: vdom$,
-    MUSIC$: music$,
+    MUSIC$: speaker.MUSIC$,
   }
 }
+
