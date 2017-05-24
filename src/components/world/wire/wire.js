4771a5669fcf38aea028654a68fc7146dcccea75
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2017-05-24 15:12:32 +0200

    review

--- src/components/world/wire/wire.js
+++ src/components/world/wire/wire.js
@@ -1,18 +1,18 @@
 import { div, img } from '@cycle/dom'
 import xs from 'xstream'
-import { ANIMATION_TIMEOUT, STOP_EVENT } from '../../../constants'
+import { ANIMATION_TIMEOUT } from '../../../constants'
 import { addDelay } from '../../../utils'
 
 const STEPS = 15
 const STEP_TIMEOUT = ANIMATION_TIMEOUT / STEPS
-const step = (a, i) => (a ? i * 2 : 0)
-const flow = i => (i % 2 === 0 ? 1 : -1)
-const translateX = (a, i) => `translateX(${step(a, i)}vw) translateY(${flow(i)}vh)`
-const translateY = (a, i) => `translateY(${step(a, i)}vh) translateX(${flow(i)}vw)`
-const style = (animate, direction, i) => ({
-  visibility: animate ? 'visible' : 'hidden',
+const getStep = (stop, step) => (stop ? 0 : step * 2)
+const flow = step => (step % 2 === 0 ? step : -1)
+const translateX = (stop, step) => `translateX(${getStep(stop, step)}vw) translateY(${flow(step)}vh)`
+const translateY = (stop, step) => `translateY(${getStep(stop, step)}vh) translateX(${flow(step)}vw)`
+const style = ({ stop, step, type }) => ({
+  visibility: stop ? 'hidden' : 'visible',
   transition: `transform ${STEP_TIMEOUT}ms`,
-  transform: animate && direction === 'top' ? translateX(animate, i) : translateY(animate, -i),
+  transform: !stop && type === 'musics' ? translateX(stop, step) : translateY(stop, -step),
 })
 
 const model = actions => (
@@ -28,17 +28,16 @@ const model = actions => (
     .map(s => ({
       ...s,
       ...event,
-      direction: event.type === 'musics' ? 'top' : 'right',
     }))
   })
   .flatten()
-  .startWith(STOP_EVENT)
+    .startWith({ stop: true })
 )
 
 const view = state$ =>
   state$.map(state => div(`.wire ${state.type} ${state.stop && '.stop'}`, [
     img({
-      style: style(!state.stop, state.direction, state.step),
+      style: style(state),
       props: { src: `/svg/notes/${state.type}.svg` },
     }),
   ]))
